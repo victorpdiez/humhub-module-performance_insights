0bbcccd8e4e0ef2c831ce68f879953edd641d580
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-03-21 09:49:48 +0900

    Update TabMenu.php

--- widgets/TabMenu.php
+++ widgets/TabMenu.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace humhub\modules\performance_insights\widgets;
 
 use Yii;
@@ -7,32 +8,32 @@ use yii\helpers\Url;
 /**
  *  Render TabMenu in Admin section
  */
-class TabMenu extends \humhub\widgets\BaseMenu 
+class TabMenu extends \humhub\widgets\BaseMenu
 {
 
   public $template = "@humhub/widgets/views/tabMenu";
   public $type = "adminUserSubNavigation";
 
-  public function init() 
+  public function init()
   {
-   $this->addItem(array(
+   $this->addItem([
     'label' => Yii::t('PerformanceInsightsModule.base', 'Analyze'),
     'url' => Url::to(['/performance_insights/admin/test']),
     'sortOrder' => 100,
     'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'performance_insights' && Yii::$app->controller->id == 'admin'&& Yii::$app->controller->action->id == 'test'),
-  ));
-   $this->addItem(array(
+  ]);
+   $this->addItem([
     'label' =>Yii::t('PerformanceInsightsModule.base', 'Settings'),
     'url' => Url::to(['/performance_insights/admin/index']),
     'sortOrder' => 101,
     'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'performance_insights' && Yii::$app->controller->id == 'admin' && Yii::$app->controller->action->id == 'index'),
-  ));   
-   $this->addItem(array(
+  ]);
+   $this->addItem([
     'label' =>Yii::t('PerformanceInsightsModule.base', 'Test History'),
     'url' => Url::to(['/performance_insights/admin/test-history']),
     'sortOrder' => 102,
     'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'performance_insights' && Yii::$app->controller->id == 'admin' && Yii::$app->controller->action->id == 'test-history'),
-  ));   
+  ]);
 
    parent::init();
  }
