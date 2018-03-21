a16d66eacdaaa0f8ba75b33bc0160c3a94b0f126
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-03-21 09:51:15 +0900

    Update loader-menu.php

--- widgets/views/loader-menu.php
+++ widgets/views/loader-menu.php
@@ -1,8 +1,8 @@
-<div class="loading loading-image" id="<?php echo $id; ?>" style="display:none;">
-	<div>
-		<div class="c1"></div>
-		<div class="c2"></div>
-		<div class="c3"></div>
-		<div class="c4"></div>
-	</div>
+<div class="loading loading-image" id="<?= $id; ?>" style="display:none;">
+    <div>
+        <div class="c1"></div>
+        <div class="c2"></div>
+        <div class="c3"></div>
+        <div class="c4"></div>
+    </div>
 </div>
