84679fad8e8abe028f94deeb1c498d1c25c0e0a5
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-03-21 09:57:11 +0900

    Update index_content.php

--- views/admin/index_content.php
+++ views/admin/index_content.php
@@ -1,11 +1,12 @@
 <?php
+
 use yii\helpers\Html;
 use yii\helpers\Url;
+
 humhub\modules\performance_insights\Assets::register($this);
 ?>
 
 <!-- Bootstrap alert message -->
-
 
 <?php if (Yii::$app->session->hasFlash('success')): ?>
 	<div class="alert alert-success alert-dismissable">
@@ -14,37 +15,36 @@ humhub\modules\performance_insights\Assets::register($this);
 	</div>
 <?php endif; ?>
 
-
 <!-- Button groups for generating space -->
 
-<label><?php echo Yii::t('PerformanceInsightsModule.base', 'Generate fake data for testing.');  ?></label>
+<label><?= Yii::t('PerformanceInsightsModule.base', 'Generate fake data for testing.');  ?></label>
 <hr>
 
-<label class="label-margin"><?php echo Yii::t('PerformanceInsightsModule.base', 'Generate Spaces') ?></label>
+<label class="label-margin"><?= Yii::t('PerformanceInsightsModule.base', 'Generate Spaces') ?></label>
 <div class="btn_group">
 	<button type="button" class="btn btn-default apply_margin" data-value="5" data-type="space">500</button>
 	<button type="button" class="btn btn-default apply_margin" data-value="1000" data-type="space">1k</button>
 	<button type="button" class="btn btn-default apply_margin" data-value="5000" data-type="space">5k</button>
 	<button type="button" class="btn btn-default apply_margin" data-value="10000" data-type="space">10k</button>
 	<button type="button" class="btn btn-default apply_margin" data-value="20000" data-type="space">20k</button>
-	<button type="button" class="btn btn-default apply_margin" data-value="50000" data-type="space">50k&nbsp;(<?php echo Yii::t('PerformanceInsightsModule.base', 'not recommended') ?>)</button>
+	<button type="button" class="btn btn-default apply_margin" data-value="50000" data-type="space">50k&nbsp;(<?= Yii::t('PerformanceInsightsModule.base', 'not recommended') ?>)</button>
 
 	<?php if($isDeleteSpaceButtonHidden): ?>
-		<button type="button" class="btn btn-danger delete_test_space" data-url="<?php echo Url::to(['/performance_insights/admin/delete']); ?>" data-type="space"><i class="fa fa-trash-o" aria-hidden="true"></i>&nbsp;<?php echo  Yii::t('PerformanceInsightsModule.base', 'Delete All Test Spaces') ?></button>
+		<button type="button" class="btn btn-danger delete_test_space" data-url="<?= Url::to(['/performance_insights/admin/delete']); ?>" data-type="space"><i class="fa fa-trash-o" aria-hidden="true"></i>&nbsp;<?=  Yii::t('PerformanceInsightsModule.base', 'Delete All Test Spaces') ?></button>
 	<?php endif; ?>
 </div>
 
 <!-- Button groups for generating user -->
 
-<label class="label-margin"><?php echo Yii::t('PerformanceInsightsModule.base', 'Generate Users') ?></label>
+<label class="label-margin"><?= Yii::t('PerformanceInsightsModule.base', 'Generate Users') ?></label>
 <div class="btn_group">
 	<button type="button" class="btn btn-default apply_margin" data-value="10" data-type="user">10k</button>
 	<button type="button" class="btn btn-default apply_margin" data-value="50000" data-type="user">50k</button>
 	<button type="button" class="btn btn-default apply_margin" data-value="100000" data-type="user">100k</button>
 	<button type="button" class="btn btn-default apply_margin" data-value="500000" data-type="user">500k</button>
-	<button type="button" class="btn btn-default apply_margin" data-value="1000000" data-type="user">1m&nbsp;(<?php echo Yii::t('PerformanceInsightsModule.base', 'not recommended')?>)</button>
+	<button type="button" class="btn btn-default apply_margin" data-value="1000000" data-type="user">1m&nbsp;(<?= Yii::t('PerformanceInsightsModule.base', 'not recommended')?>)</button>
 	<?php if($isDeleteUserButtonHidden): ?>
-		<button type="button" class="btn btn-danger delete_test_user" data-url="<?php echo Url::to(['/performance_insights/admin/delete']); ?>" data-type="user"><i class="fa fa-trash-o" aria-hidden="true"></i>&nbsp; <?php echo Yii::t('PerformanceInsightsModule.base', 'Delete All Test Users') ?></button>
+		<button type="button" class="btn btn-danger delete_test_user" data-url="<?= Url::to(['/performance_insights/admin/delete']); ?>" data-type="user"><i class="fa fa-trash-o" aria-hidden="true"></i>&nbsp; <?php echo Yii::t('PerformanceInsightsModule.base', 'Delete All Test Users') ?></button>
 	<?php endif; ?>
 </div>
 
@@ -57,7 +57,6 @@ humhub\modules\performance_insights\Assets::register($this);
 <input type='hidden' id="test-quantity" name='number' style="display:none;">
 <input type='submit' name='submit' style="display:none;">
 <?= \yii\helpers\Html::endForm() ?>
-
 
 <!-- initialize script -->
 
